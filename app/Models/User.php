3bdaf3d181a0164b91d6a337b202fdc15d25469e
boostedgagi <dragan.02jelic@gmail.com>
2026-01-31 11:07:19 +0100

    Minor, user created.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    /**
+     * @use HasFactory<UserFactory>
+     */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'password',
+        'isAllowed',
+        'reputation',
+        'roles'
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'isAllowed' => 'boolean',
+        'reputation' => 'integer',
+        'roles' => 'array',
+        'createdAt' => 'datetime',
+        'updatedAt' => 'datetime'
+    ];
+
 }
