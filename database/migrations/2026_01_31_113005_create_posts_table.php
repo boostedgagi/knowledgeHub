c4eeb28024c676e28b1b530c4fc9b7e490a209f7
boostedgagi <dragan.02jelic@gmail.com>
2026-01-31 17:46:58 +0100

    Post seeder made and minor changes on posts migration

--- database/migrations/2026_01_31_113005_create_posts_table.php
+++ database/migrations/2026_01_31_113005_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,7 @@ return new class extends Migration
             $table->foreignId('categoryId')->nullable()->constrained('categories','id')->nullOnDelete();
             $table->foreignId('userId')->constrained('users','id')->cascadeOnDelete();
             $table->foreignId('tagId')->nullable()->constrained('tags','id')->nullOnDelete();
-            $table->dateTime('createdAt');
+            $table->dateTime('createdAt')->default(Carbon::now()->format('Y-m-d H:i:s'));
             $table->dateTime('updatedAt')->nullable();
         });
     }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('posts')->insert([
+            'content' => Str::random(50),
+            'upVotes' => 25,
+            'downVotes' => 5,
+            'categoryId' => 1,
+            'userId'=>1
+        ]);
     }
+
 }
