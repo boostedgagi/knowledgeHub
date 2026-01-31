ee296d34d7f311c2764ec18c7220dcb78c6f0d01
boostedgagi <dragan.02jelic@gmail.com>
2026-01-31 16:24:41 +0100

    Cache related migration,

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UserController::class, 'showAll']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'register']);
+
+
