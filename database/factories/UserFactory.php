26886dce4aebd71e14167171b377cfd07939343d
boostedgagi <dragan.02jelic@gmail.com>
2026-01-31 15:33:48 +0100

    User factory and minor changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+
+    protected static function run()
+    {
+        User::factory()->count(20)->make();
+    }
 
     /**
      * Define the model's default state.
@@ -24,21 +25,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstName' => fake()->name(),
+            'lastName' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'isAllowed' => fake()->boolean,
+            'reputation' => fake()->numberBetween(1, 99),
+            'roles' => fake()->word,
+            'createdAt' => fake()->dateTime
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
