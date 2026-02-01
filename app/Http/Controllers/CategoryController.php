0e72e7803ec1654474cdfd64d6cac998438c98d2
boostedgagi <dragan.02jelic@gmail.com>
2026-02-01 16:08:42 +0100

    Category controller crud methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -24,11 +25,46 @@ class CategoryController
 
     /**
      * POST /categories
-     * @return void
+     * @return JsonResponse
      */
     public function create(Request $request){
         $category = DB::table('categories')->insert([
             'title' => $request->input('title')
         ]);
+
+        return response()->json(
+            $category,
+            201
+        );
+    }
+
+    /**
+     * PUT /categories/{id}
+     * @return JsonResponse
+     */
+    public function update(int $id, Request $request){
+        $categoryToEdit = DB::table('categories')->where('id', $id);
+        $categoryToEdit->update($request->all());
+
+        $categoryToEdit->update(['updatedAt' => Carbon::now()->format('Y-m-d H:i:s')]);
+
+        return response()->json(
+            $categoryToEdit->first(),
+            201
+        );
+    }
+
+    /**
+     * DELETE /categories/{id}
+     * @param int $
+     * @return JsonResponse
+     */
+    public function delete(int $id){
+        DB::table('categories')->delete($id);
+
+        return response()->json(
+            [],
+            204
+        );
     }
 }
