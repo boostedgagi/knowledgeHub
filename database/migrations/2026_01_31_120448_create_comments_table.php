659c52677798feb2786f7658d62d6b6a457d4e44
boostedgagi <dragan.02jelic@gmail.com>
2026-02-02 00:21:27 +0100

    Minor changes on migrations

--- database/migrations/2026_01_31_120448_create_comments_table.php
+++ database/migrations/2026_01_31_120448_create_comments_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('content');
+            $table->integer('approvedByOwner')->default(0);
             $table->foreignId('parentCommentId')->nullable()->constrained('comments','id')->cascadeOnDelete();
             $table->foreignId('userId')->nullable()->constrained('users','id')->cascadeOnDelete();
             $table->foreignId('postId')->nullable()->constrained('posts','id')->cascadeOnDelete();
