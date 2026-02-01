176b88dc9e3804923d262e66b58c589dc3ab8709
boostedgagi <dragan.02jelic@gmail.com>
2026-02-01 12:47:09 +0100

    Minor change in categories migration

--- database/migrations/2026_01_31_112035_create_categories_table.php
+++ database/migrations/2026_01_31_112035_create_categories_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->dateTime('createdAt');
+            $table->dateTime('createdAt')->default(Carbon::now()->format('Y-m-d H:i:s'));
             $table->dateTime('updatedAt')->nullable();
         });
     }
